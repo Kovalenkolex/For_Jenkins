c3e7e102ed18c3a8ecc4b3998d6c66989c854c89
kolex <kovalenkolex63@gmail.com>
2024-09-09 11:34:44 +0300

    cheh

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import requests
 
-# Функция для получения случайного анекдота
+# Функция для получения случайного анекдота string
 def get_joke():
     url = "https://v2.jokeapi.dev/joke/Any?type=single"
     response = requests.get(url)
